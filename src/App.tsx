bb5dac7460833c77c700d222a1f6a55281321bf4
tothmatyas <tothmatyas621@gmail.com>
2026-01-13 11:37:11 +0100

    Vége

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from "react"
+
 const App = () => {
+  const [random,setRandom] = useState(Math.floor(Math.random()*10))
+  const [startTime,] = useState(0)
+  const [current, setCurrent] = useState<number>(startTime)
+  const [isStopped,setIsStopped] = useState(false)
+  const [waitTime, setWaitTime] = useState(0)
+
+useEffect(()=>{
+    if(isStopped) return
+
+    const intervalId = setInterval(() => {
+        setCurrent(prev => (prev + 1))       
+    }, 10)
+    
+    return () => {clearInterval(intervalId)}
+
+
+  }, [isStopped])
+
+  useEffect(()=>{
+    setInterval(() => {
+      setWaitTime(prev=>(prev + 1))
+    }, 1000)
+  },[isStopped])
+
   return (
-    <div>App</div>
+    <div>
+      {waitTime > random ? 
+      <div id="zold">
+        <p>Elért eredmény:</p>
+      {current -(random*100)}ms
+      <button id="stopBtn" onClick={()=>{setIsStopped(prev => !prev)}}>Ide kell kattintani</button>
+      </div>
+      :
+      <div id="sarga">
+      <h1>Reflexjáték</h1>
+      <p>zöldnél kattints</p>
+      {waitTime}
+      </div>
+    }
+    </div>
   )
 }
 
